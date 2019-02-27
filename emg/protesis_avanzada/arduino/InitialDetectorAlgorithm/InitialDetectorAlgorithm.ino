f29fe690194fa4e3bf5280924825b4b5b468b8c1
Bryn Davis <bryn_davis@hotmail.com>
2019-02-27 14:41:47 -0500

    Implementing filtering from the OYMotion sensor, but the cutoff frequencies aren't exposed.  Not really too useful.

--- emg/protesis_avanzada/arduino/InitialDetectorAlgorithm/InitialDetectorAlgorithm.ino
+++ emg/protesis_avanzada/arduino/InitialDetectorAlgorithm/InitialDetectorAlgorithm.ino
@@ -11,25 +11,45 @@
   Bryn simply modified the analog pin to use a pass through on the FlexVolt
 */
 
+#include "EMGFilters.h"
+
+#define SensorInputPin A5 // input pin number
+
+EMGFilters paFilter;
+int sampleRate = SAMPLE_FREQ_500HZ;
+int humFreq = NOTCH_FREQ_60HZ;
+
 float threshold = 1.5;     // Threshold to compare to
 int state = 0;           // 0 for no signal; 1 for signal
 
 // the setup routine runs once when you press reset:
 void setup() {
+  // get the filter going
+  paFilter.init(sampleRate, humFreq, false, false, true);
+  
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 5:
-  int sensorValue = analogRead(A5);
+  
+  // read the input:
+  int sensorValue = analogRead(SensorInputPin);
+  
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float voltage = sensorValue * (5.0 / 1023.0);
+
+  // Update the filter
+  int DataAfterFilter = paFilter.update(sensorValue);
+  float FilteredData = DataAfterFilter * (5.0 / 1023.0);
+  
   // print out the value you read:
   Serial.print(voltage);
   Serial.print("   ");
   if (voltage > threshold) {state = 1;}
   else {state = 0;}
-  Serial.println(state);
+  Serial.print(state);
+  Serial.print("   ");
+  Serial.println(FilteredData);
 }
