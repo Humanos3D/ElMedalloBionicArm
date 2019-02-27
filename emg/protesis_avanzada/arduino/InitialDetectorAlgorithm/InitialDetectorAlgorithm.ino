1a34ff120ef9412734a389d769ccc5ef546c14dd
Bryn Davis <bryn_davis@hotmail.com>
2019-02-27 12:29:22 -0500

    Starting a place to work on the signal detection algorithm.  Just copying the basic open-source reader for the PA sensor to begin with.

---
+++ emg/protesis_avanzada/arduino/InitialDetectorAlgorithm/InitialDetectorAlgorithm.ino
@@ -0,0 +1,28 @@
+/*
+  ReadAnalogVoltage
+
+  Reads an analog input on pin 5, converts it to voltage, and prints the result to the Serial Monitor.
+  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/ReadAnalogVoltage
+
+  Bryn simply modified the analog pin to use a pass through on the FlexVolt
+*/
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 5:
+  int sensorValue = analogRead(A5);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5.0 / 1023.0);
+  // print out the value you read:
+  Serial.println(voltage);
+}
