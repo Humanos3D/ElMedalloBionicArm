bebe5ea395da0c0ac7522b145a802840bda46333
Bryn Davis <bryn_davis@hotmail.com>
2019-02-27 13:00:10 -0500

    Very simple thresholding routine (depends only on current sample) that demonstrates we can see and debug using the Arduino serial plotter.

--- emg/protesis_avanzada/arduino/InitialDetectorAlgorithm/InitialDetectorAlgorithm.ino
+++ emg/protesis_avanzada/arduino/InitialDetectorAlgorithm/InitialDetectorAlgorithm.ino
@@ -11,6 +11,9 @@
   Bryn simply modified the analog pin to use a pass through on the FlexVolt
 */
 
+float threshold = 1.5;     // Threshold to compare to
+int state = 0;           // 0 for no signal; 1 for signal
+
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -24,5 +27,9 @@ void loop() {
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float voltage = sensorValue * (5.0 / 1023.0);
   // print out the value you read:
-  Serial.println(voltage);
+  Serial.print(voltage);
+  Serial.print("   ");
+  if (voltage > threshold) {state = 1;}
+  else {state = 0;}
+  Serial.println(state);
 }
