f116a6228a513ad97030a8ba0df8a26e62489030
Bryn Davis <bryn_davis@hotmail.com>
2019-03-13 14:52:35 -0500

    A bit of a tidy up of the EMG algorithm, including optional output of the debug signals and (crude) rate monitoring

--- emg/detection_algorithm/InitialDetectorAlgorithm/InitialDetectorAlgorithm.ino
+++ emg/detection_algorithm/InitialDetectorAlgorithm/InitialDetectorAlgorithm.ino
@@ -13,23 +13,24 @@
 #define SensorInputPin A5 // input pin number
 
 // Setup parameters
-int sensor = 1;                   // 0 for Protesis Avanzada; 1 for OYMotion
+int sensor = 0;                   // 0 for Protesis Avanzada; 1 for OYMotion
+int debug_signals = 1;            // 1 to show signals over serial
 
 // Fixed parameters
 float background_frequency = 0.2; // Change rate to be considered background (Hz)
-float ceiling_frequency = 300;    // Highest expected frequency (Hz)
+float ceiling_frequency = 150;    // Highest expected frequency (Hz)
 float fall_time = 1000;           // Signal must be low this long for 1 -> 0 (ms)
 
 // Variable parameters we'll change in setup
-float threshold;                     // Set later
-float rise_time;                     // Set later
-float background_timeout;            // Set later
-float threshold_oy = .25;            // Voltage above background to register signal
-float rise_time_oy = 2;              // Must see signal this long for 0 -> 1 (ms)
-float background_timeout_oy = 5000;  // Max time to not calculate background (ms)
-float threshold_pa = .5;             // Voltage above background to register signal
-float rise_time_pa = 100;            // Must see signal this long for 0 -> 1 (ms)
-float background_timeout_pa = 5000;  // Max time to not calculate background (ms)
+float threshold;                     // Voltage above background to register signal
+float rise_time;                     // Must see signal this long for 0 -> 1 (ms)
+float background_timeout;            // Max time to not calculate background (ms)
+float threshold_oy = .25;            
+float rise_time_oy = 2;      
+float background_timeout_oy = 5000;  
+float threshold_pa = .5;         
+float rise_time_pa = 100;    
+float background_timeout_pa = 5000; 
 
 // Initialization
 int state = 0;                 // 0 for no signal; 1 for signal
@@ -38,6 +39,9 @@ bool high_now = false;         // Whether the instaneous signal is high
 int last_low = 0;              // Time (ms) of last observed low
 int last_high = 0;             // Time (ms) of last observed high
 int last_background = 0;       // Time (ms) of contributing background
+int current_time = 0;          // Tracking loop time (ms)
+int previous_time = 0;
+float emg_signal;               // Current signal 
 
 // create a one pole filter to estimate background
 FilterOnePole backgroundFilter(LOWPASS, background_frequency); 
@@ -73,20 +77,25 @@ void loop() {
   
   // read the input:
   int sensorValue = analogRead(SensorInputPin);
-  int current_time = millis();
+  current_time = millis();
 
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float voltage = sensorValue * (5.0 / 1023.0);
 
   // do low pass filtering
-  float lp_signal = lowpassFilter.input(voltage);
+  if (ceiling_frequency == 0){     // Do nothing
+    emg_signal = voltage;
+  }
+  else {                           // Filter out high frequencies
+    emg_signal = lowpassFilter.input(voltage);    
+  }
 
   // Do accounting based on whether we are exceeding threshold
   if (sensor == 0){
-    high_now = (lp_signal - background) > threshold;
+    high_now = (emg_signal - background) > threshold;
   }
   else if (sensor == 1){
-    high_now = abs(lp_signal - background) > threshold;
+    high_now = abs(emg_signal - background) > threshold;
   }
   if (high_now) {
     last_high = current_time;
@@ -109,22 +118,30 @@ void loop() {
 
   // Track the background
   if ((state == 0) and not high_now){
-    background = backgroundFilter.input(lp_signal);
+    background = backgroundFilter.input(emg_signal);
     last_background = current_time;
     }
   else if (current_time - last_background > background_timeout){
-    background = backgroundFilter.input(lp_signal);      
+    background = backgroundFilter.input(emg_signal);      
   }
 
-  // Print out the system values
-  Serial.print(lp_signal);
-  Serial.print("   ");
-  Serial.print(state);
-  Serial.print("   ");
-//  Serial.print(high_now);
-//  Serial.print("   ");
-  Serial.println(background);
+  // Print out the signal values
+  if (debug_signals == 1){
+     Serial.print(emg_signal);
+     Serial.print("   ");
+     Serial.print(state);
+     Serial.print("   ");
+  // Serial.print(high_now);
+  // Serial.print("   ");
+     Serial.print(background);
+     Serial.print("   ");
+  }
 
+  // Track the speed (in hundreds of Hz to keep similar scale)
+  float rate = 10. / (current_time - previous_time);
+  previous_time = current_time;
+  Serial.println(rate);
+  
   // Control the digital outputs
   if (state == 1){digitalWrite(LED_BUILTIN, HIGH);}
   else {digitalWrite(LED_BUILTIN, LOW);}
