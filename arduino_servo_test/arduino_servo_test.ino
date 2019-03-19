31a43e4d1e667032107594c06bbe9fa29a7560a9
Mark Walbran <mark.walbran@gmail.com>
2019-03-19 15:26:27 -0500

    Added demo arduino code using servo motor on robotic arm iteration 1.
    Servo motor currently controlled by toggle button, which will need to be replaced by digital output from EMG signal processing in the future.

---
+++ arduino_servo_test/arduino_servo_test.ino
@@ -0,0 +1,50 @@
+// Include the Servo library
+#include <Servo.h>
+
+// Define pins and constants
+const int servoPin = 3;
+const int buttonPin = 2;
+const int OPEN_POS = 0;
+const int CLOSED_POS = 80;
+const int delayTime = 500;
+
+// Initialise variables
+boolean buttonState = LOW;
+boolean buttonFlag = LOW;
+int motorValue = OPEN_POS;
+
+// Create a servo object
+Servo Servo1;
+
+void setup() {
+  // Initialise servo and button mode
+  Servo1.attach(servoPin);
+  Servo1.write(motorValue);
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  //Debugging
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // Read button value
+  buttonState = digitalRead(buttonPin);
+
+  // Toggle motor position if button is pressed
+  if (buttonState == HIGH) {
+    Serial.println("Button Pushed"); // Debugging
+    if (buttonFlag == LOW) {
+      motorValue = CLOSED_POS;
+      Servo1.write(motorValue);
+      Serial.println("Closing..."); // Debugging
+      buttonFlag = HIGH;
+    } else if (buttonFlag == HIGH) {
+      motorValue = OPEN_POS;
+      Servo1.write(motorValue);
+      Serial.println("Opening..."); // Debugging
+      buttonFlag = LOW;
+    }
+    delay(delayTime);
+  }
+}
